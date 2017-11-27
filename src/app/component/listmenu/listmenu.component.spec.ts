47b13e6956346c941d75e5bc29b1938c16722ff3
marikja7 <marikja7@fit.cvut.cz>
2017-11-27 12:33:40 +0100

    Tests

--- src/app/component/listmenu/listmenu.component.spec.ts
+++ src/app/component/listmenu/listmenu.component.spec.ts
@@ -1,16 +1,27 @@
+// import { SearchFriendComponent } from '../search-friend/search-friend.component';
 // import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 // import { ListmenuComponent } from './listmenu.component';
+// import { NgModel } from '@angular/forms/src/directives';
+// import { FormsModule } from '@angular/forms';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { AngularFireDatabase } from 'angularfire2/database';
+// import { AngularFireModule } from 'angularfire2';
+// import { firebaseConfig } from '../../../environments/firebase.config';
 
 // describe('ListmenuComponent', () => {
 //   let component: ListmenuComponent;
 //   let fixture: ComponentFixture<ListmenuComponent>;
 
 //   beforeEach(async(() => {
+  
 //     TestBed.configureTestingModule({
-//       declarations: [ ListmenuComponent ]
+//       imports:[FormsModule, NgbModule.forRoot(), AngularFireModule.initializeApp(firebaseConfig),],
+//       providers: [  ],
+//       declarations: [ ListmenuComponent, SearchFriendComponent ]
 //     })
 //     .compileComponents();
+    
 //   }));
 
 //   beforeEach(() => {
--- src/app/component/search-user/search-user.component.spec.ts
+++ src/app/component/search-user/search-user.component.spec.ts
@@ -1,16 +1,23 @@
+// import { FirebaseApp } from 'angularfire2/firebase.app.module';
 // import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 // import { SearchUserComponent } from './search-user.component';
+// import { AngularFireAuth } from 'angularfire2/auth';
+// import { NgModule } from '@angular/core';
+
 
 // describe('SearchUserComponent', () => {
 //   let component: SearchUserComponent;
 //   let fixture: ComponentFixture<SearchUserComponent>;
 
 //   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ SearchUserComponent ]
-//     })
-//     .compileComponents();
+ 
+//       TestBed.configureTestingModule({
+//         declarations: [SearchUserComponent],
+//         imports: [ ],
+//         providers: [AngularFireAuth, FirebaseApp ]
+//       })
+//         .compileComponents();
 //   }));
 
 //   beforeEach(() => {
@@ -19,7 +26,8 @@
 //     fixture.detectChanges();
 //   });
 
-//   it('should create', () => {
+//   it('SearchUser component should create', () => {
 //     expect(component).toBeTruthy();
 //   });
+
 // });
